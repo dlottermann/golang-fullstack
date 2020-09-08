4a17d199f9ebe22c1c66f2a21bb86f2d65ce272c
diego.lottermann <diego.lottermann@compasso.com.br>
2020-09-08 12:02:34 -0300

    feat:Add frontend initial structure

---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+          <Switch>
+            <Route exact path="/">
+              <App />
+            </Route>
+          </Switch>
+        </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
